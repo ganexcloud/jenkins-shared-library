e2b2c28303c38a01a3b297f7d8d59cdf914b9a7c
caiohasouza <caio@ganex.com.br>
2023-07-17 10:29:32 -0300

    chore: add new files

---
+++ src/org/ganex/jenkins/discord/Color.groovy
@@ -0,0 +1,22 @@
+package org.ganex.jenkins.discord
+
+
+String red() {
+  return 'danger'
+}
+
+String green() {
+  return 'good'
+}
+
+String yellow() {
+  return '#DAA038'
+}
+
+String blue() {
+  return '#1f78c1'
+}
+
+String gray() {
+  return '#808080'
+}
---
+++ src/org/ganex/jenkins/discord/SlackSender.groovy
@@ -0,0 +1,25 @@
+package org.ganex.jenkins.discord
+
+
+void send(String message, String color = '#fff') {
+  def options = getOptions message, color
+
+  discordSend options
+}
+
+
+def getOptions(String message = '', String color = '') {
+  def obj = [
+    message: message
+  ]
+
+  if (color) {
+    obj.color = color
+  }
+
+  if (env.discord_WEBHOOK_URL) {
+    obj.webhookURL = env.discord_WEBHOOK_URL
+  }
+
+  return obj
+}
