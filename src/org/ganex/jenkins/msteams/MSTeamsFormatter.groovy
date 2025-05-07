40df40a30b8dfed943d83dcc2639d644b6ad2bcd
caiohasouza <caio@ganex.com.br>
2025-05-07 14:49:58 -0300

    fix: msteams sintax (#5)

--- src/org/ganex/jenkins/msteams/MSTeamsFormatter.groovy
+++ src/org/ganex/jenkins/msteams/MSTeamsFormatter.groovy
@@ -1,6 +1,5 @@
 package org.ganex.jenkins.msteams
 
-
 String formatMessageSimple(String title = '') {
   def helper = new JenkinsHelper()
   def status = new JenkinsStatus()
@@ -12,48 +11,45 @@ String formatMessageSimple(String title = '') {
   def url = helper.getAbsoluteUrl()
   def templateJson = """
 {
-    "type": "message",
-    "attachments": [
-      {
-        "contentType": "application/vnd.microsoft.card.adaptive",
-        "content": {
-          "type": "AdaptiveCard",
-          "version": "1.4",
-          "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-          "msteams": {  
-            "width": "Full"  
-          },
-          "body": [
-            {
-              "type": "Container",
-              "style": "default",
-              "items": [
-                {
-                  "type": "TextBlock",
-                  "text": "${titleFormated}",
-                  "weight": "Bolder",
-                  "size": "Large"
-                },
-                {
-                  "type": "ActionSet",
-                  "actions": [
-                    {
-                      "type": "Action.OpenUrl",
-                      "title": "View Job",
-                      "url": "${url}"
-                    }
-                  ]
-                }
-              ]
-            }
-          ],
-        }
+  "type": "message",
+  "attachments": [
+    {
+      "contentType": "application/vnd.microsoft.card.adaptive",
+      "content": {
+        "type": "AdaptiveCard",
+        "version": "1.4",
+        "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+        "msteams": { "width": "Full" },
+        "body": [
+          {
+            "type": "Container",
+            "style": "default",
+            "items": [
+              {
+                "type": "TextBlock",
+                "text": "${titleFormated}",
+                "weight": "Bolder",
+                "size": "Large"
+              },
+              {
+                "type": "ActionSet",
+                "actions": [
+                  {
+                    "type": "Action.OpenUrl",
+                    "title": "View Job",
+                    "url": "${url}"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }
-    ]
-  }
-  """
-  def jsonFinal = templateJson
-  return jsonFinal
+    }
+  ]
+}
+"""
+  return templateJson
 }
 
 String formatMessage(String title = '', String message = '', String testSummary = '') {
@@ -65,52 +61,65 @@ String formatMessage(String title = '', String message = '', String testSummary 
   def titleFormated = "${project} (#${buildNumber}) - ${title.trim()}"
   def color = status.getStatusColor()
   def url = helper.getAbsoluteUrl()
-  def result = ""
-  if(message) result = "\n ${message.trim()}"
-  if(testSummary) result = "\n ${testSummary}"
+  def resultItems = ""
+
+  if (message?.trim()) {
+    resultItems += """,
+              {
+                "type": "TextBlock",
+                "text": "${message.trim()}",
+                "wrap": true
+              }"""
+  }
+
+  if (testSummary?.trim()) {
+    resultItems += """,
+              {
+                "type": "TextBlock",
+                "text": "${testSummary.trim()}",
+                "wrap": true
+              }"""
+  }
+
   def templateJson = """
 {
-    "type": "message",
-    "attachments": [
-      {
-        "contentType": "application/vnd.microsoft.card.adaptive",
-        "content": {
-          "type": "AdaptiveCard",
-          "version": "1.4",
-          "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-          "msteams": {  
-            "width": "Full"  
-          },
-          "body": [
-            {
-              "type": "Container",
-              "style": "${color}",
-              "items": [
-                {
-                  "type": "TextBlock",
-                  "text": "${titleFormated}",
-                  "weight": "Bolder",
-                  "size": "Large"
-                },
-                ${message}
-                {
-                  "type": "ActionSet",
-                  "actions": [
-                    {
-                      "type": "Action.OpenUrl",
-                      "title": "View Job",
-                      "url": "${url}"
-                    }
-                  ]
-                }
-              ]
-            }
-          ],
-        }
+  "type": "message",
+  "attachments": [
+    {
+      "contentType": "application/vnd.microsoft.card.adaptive",
+      "content": {
+        "type": "AdaptiveCard",
+        "version": "1.4",
+        "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+        "msteams": { "width": "Full" },
+        "body": [
+          {
+            "type": "Container",
+            "style": "${color}",
+            "items": [
+              {
+                "type": "TextBlock",
+                "text": "${titleFormated}",
+                "weight": "Bolder",
+                "size": "Large"
+              }${resultItems},
+              {
+                "type": "ActionSet",
+                "actions": [
+                  {
+                    "type": "Action.OpenUrl",
+                    "title": "View Job",
+                    "url": "${url}"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
       }
-    ]
-  }
-  """
-  def jsonFinal = templateJson
-  return jsonFinal
+    }
+  ]
+}
+"""
+  return templateJson
 }
